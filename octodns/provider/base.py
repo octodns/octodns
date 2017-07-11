11a246da81928640387a8af36f0b3da8a93cfbe2
Joe Williams <joe.williams@github.com>
2017-07-11 07:20:18 -0700

    whitespace

--- octodns/provider/base.py
+++ octodns/provider/base.py
@@ -58,17 +58,17 @@ class Plan(object):
             if update_pcent > self.MAX_SAFE_UPDATE_PCENT:
                 raise UnsafePlan('Too many updates, {} is over {} percent'
                                  '({}/{})'.format(
-                                 update_pcent,
-                                 self.MAX_SAFE_UPDATE_PCENT * 100,
-                                 self.change_counts['Update'],
-                                 existing_record_count))
+                                     update_pcent,
+                                     self.MAX_SAFE_UPDATE_PCENT * 100,
+                                     self.change_counts['Update'],
+                                     existing_record_count))
             if delete_pcent > self.MAX_SAFE_DELETE_PCENT:
                 raise UnsafePlan('Too many deletes, {} is over {} percent'
                                  '({}/{})'.format(
-                                 delete_pcent,
-                                 self.MAX_SAFE_DELETE_PCENT * 100,
-                                 self.change_counts['Delete'],
-                                 existing_record_count))
+                                     delete_pcent,
+                                     self.MAX_SAFE_DELETE_PCENT * 100,
+                                     self.change_counts['Delete'],
+                                     existing_record_count))
 
     def __repr__(self):
         return 'Creates={}, Updates={}, Deletes={}, Existing Records={}' \
