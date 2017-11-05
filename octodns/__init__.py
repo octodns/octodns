7fa999953fc3229f95053d0c36a6ee14f1251c63
Hervé Beraud <herveberaud.pro@gmail.com>
2017-11-05 13:38:31 +0100

    reset changes on __init__
    fix error on version

--- octodns/__init__.py
+++ octodns/__init__.py
@@ -1,5 +1,6 @@
+'OctoDNS: DNS as code - Tools for managing DNS across multiple providers'
+
 from __future__ import absolute_import, division, print_function, \
     unicode_literals
 
-
-__version__ = '0.8.8'
+__VERSION__ = '0.8.8'
