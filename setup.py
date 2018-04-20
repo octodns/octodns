f86c06d30429f7da0faab7cb0678087a3ddd31b7
Ross McFarland <ross@github.com>
2018-04-20 12:35:58 -0700

    Revert the setup.cfg infra and go back to requirements.txt
    I've continually run into problems with the setup.cfg route that I'm not
    interested in investing the time to debug and fix. Looking around it doesn't
    seem to be very common yet so I've had a hard time finding help/docs etc.

--- setup.py
+++ setup.py
@@ -1,5 +1,48 @@
 #!/usr/bin/env python
-from setuptools import setup
 
+from os.path import dirname, join
+import octodns
 
-setup()
+try:
+    from setuptools import find_packages, setup
+except ImportError:
+    from distutils.core import find_packages, setup
+
+cmds = (
+    'compare',
+    'dump',
+    'report',
+    'sync',
+    'validate'
+)
+cmds_dir = join(dirname(__file__), 'octodns', 'cmds')
+console_scripts = {
+    'octodns-{name} = octodns.cmds.{name}:main'.format(name=name)
+    for name in cmds
+}
+
+setup(
+    author='Ross McFarland',
+    author_email='rwmcfa1@gmail.com',
+    description=octodns.__doc__,
+    entry_points={
+        'console_scripts': console_scripts,
+    },
+    install_requires=[
+        'PyYaml>=3.12',
+        'dnspython>=1.15.0',
+        'futures>=3.2.0',
+        'incf.countryutils>=1.0',
+        'ipaddress>=1.0.22',
+        'natsort>=5.2.0',
+        # botocore doesn't like >=2.7.0 for some reason
+        'python-dateutil>=2.6.0,<2.7.0',
+        'requests>=2.18.4'
+    ],
+    license='MIT',
+    long_description=open('README.md').read(),
+    name='octodns',
+    packages=find_packages(),
+    url='https://github.com/github/octodns',
+    version=octodns.__VERSION__,
+)
