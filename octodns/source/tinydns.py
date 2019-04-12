a10cab351b9d324c825644cf7d3c06bda57c0f8a
Andy Hawkins <andy@gently.org.uk>
2019-04-12 21:29:58 +0100

    Add support for TinyDNS AAAA records

--- octodns/source/tinydns.py
+++ octodns/source/tinydns.py
@@ -11,6 +11,7 @@ from os import listdir
 from os.path import join
 import logging
 import re
+import textwrap
 
 from ..record import Record
 from ..zone import DuplicateRecordException, SubzoneRecordException
@@ -20,7 +21,7 @@ from .base import BaseSource
 class TinyDnsBaseSource(BaseSource):
     SUPPORTS_GEO = False
     SUPPORTS_DYNAMIC = False
-    SUPPORTS = set(('A', 'CNAME', 'MX', 'NS', 'TXT'))
+    SUPPORTS = set(('A', 'CNAME', 'MX', 'NS', 'TXT', 'AAAA'))
 
     split_re = re.compile(r':+')
 
@@ -35,6 +36,20 @@ class TinyDnsBaseSource(BaseSource):
                 values.append(record[0])
         if len(values) == 0:
             return
+        try:
+            ttl = records[0][1]
+        except IndexError:
+            ttl = self.default_ttl
+        return {
+            'ttl': ttl,
+            'type': _type,
+            'values': values,
+        }
+
+    def _data_for_AAAA(self, _type, records):
+        values = []
+        for record in records:
+            values.append(u":".join(textwrap.wrap(record[0], 4)))
         try:
             ttl = records[0][1]
         except IndexError:
@@ -121,6 +136,8 @@ class TinyDnsBaseSource(BaseSource):
             '+': 'A',
             '@': 'MX',
             '\'': 'TXT',
+            '3': 'AAAA',
+            '6': 'AAAA',
         }
         name_re = re.compile(r'((?P<name>.+)\.)?{}$'.format(zone.name[:-1]))
 
