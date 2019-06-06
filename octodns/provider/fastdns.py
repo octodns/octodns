e1a139d18753ecb7f68acf7c9a051c686fe5332d
Basir Jamil <basir.jamil@microsoft.com>
2019-06-06 14:31:26 -0400

    revamed authentication

--- octodns/provider/fastdns.py
+++ octodns/provider/fastdns.py
@@ -9,6 +9,8 @@ from __future__ import absolute_import, division, print_function, \
 from os.path import expanduser
 import ConfigParser
 
+# import os ## command line workaround
+
 
 import logging
 from functools import reduce
@@ -42,65 +44,57 @@ class AkamaiProvider(BaseProvider):
 
     def _authenticate(self, client_secret, host, access_token, client_token)
 
-        ## generate edgegrid
-        section_name = "dns"
-        home = expanduser("~")
-        index = 0 
-        fields = {}
+        # ## generate edgegrid
+        # section_name = "dns"
+        # home = expanduser("~")
+        # index = 0 
+        # fields = {}
 
-        ## process original .edgerc file
-        origConfig = ConfigParser.ConfigParser()
-        filename = "%s/.edgerc" % home
+        # ## process original .edgerc file
+        # origConfig = ConfigParser.ConfigParser()
+        # filename = "%s/.edgerc" % home
         
-        # If this is a new file, create it
-        if not os.path.isfile(filename):
-            open(filename, 'a+').close()
+        # # If this is a new file, create it
+        # if not os.path.isfile(filename):
+        #     open(filename, 'a+').close()
             
-        origConfig.read(filename)
+        # origConfig.read(filename)
+
+        # ## credential type already exists, prompt to overwrite
+        # if section_name in origConfig.sections():
+        #     print (">>> Replacing section: %s" % section_name)
+        #     sys.stdout.write ("*** OK TO REPLACE section %s? *** [Y|n]:" % section_name)
+        #     real_raw_input = vars(__builtins__).get('raw_input',input)
+        #     choice = real_raw_input().lower()
+        #     if choice == "n":
+        #         print ("Not replacing section.")
+        #         return
+        #     replace_section = True
+        # else:
+        #     print ("+++ Creating section: %s" % section_name)
+        #     replace_section = False
+
+        # ## open the ~/.edgerc file for writing
+        # Config = ConfigParser.ConfigParser()
+        # Config.read(filename)
+        # confile = open(filename, 'w')
 
 
-        if section_name in origConfig.sections():
-            print (">>> Replacing section: %s" % section_name)
-            
-
-
-            sys.stdout.write ("*** OK TO REPLACE section %s? *** [Y|n]:" % section_name)
-            real_raw_input = vars(__builtins__).get('raw_input',input)
-            choice = real_raw_input().lower()
-            if choice == "n":
-                print ("Not replacing section.")
-                exit(0)
-
-            replace_section = True
-        else:
-            print ("+++ Creating section: %s" % section_name)
-            replace_section = False
+        # ## add  the new section
+        
 
 
         #### create credential file, and write credentials to it
 
+        with open('~/.edgerc', 'a') as credFile:
 
-        credFile = open('tempCred.txt', 'w')
+            credFile.write('[dns]\n')
 
-        credFile.write('client_secret = ')
-        credFile.write(str(client_secret))
-        credFile.write('\n\n')
-
-        credFile.write('host = ' + str(host))
-        credFile.write('\n\n')
-
-        credFile.write('access_token = ' + str(access_token))
-        credFile.write('\n\n')
-        
-        credFile.write('client_token = ' + str(client_token))
-        credFile.write('\n')
-
-        credFile.close()
-
-         
-        #### generate edgegrid using tool
-
-        #### delete temp txt file 
+            credFile.write('client_secret = ' + str(client_secret) + '\n')
+            credFile.write('host = ' + str(host) + '\n')
+            credFile.write('access_token = ' + str(access_token) + '\n')
+            credFile.write('client_token = ' + str(client_token) + '\n')
+            credFile.close()
 
     def populate(self, zone, target=False, lenient=False):
         pass
