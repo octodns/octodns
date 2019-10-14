759c44f35be46f50e87ed9dc7022c177504a4ecc
Ross McFarland <ross@github.com>
2019-10-14 08:39:45 -0700

    EqualityTupleMixin needs an explicit inhert from object to make 2.7 happy

--- octodns/equality.py
+++ octodns/equality.py
@@ -6,7 +6,7 @@ from __future__ import absolute_import, division, print_function, \
     unicode_literals
 
 
-class EqualityTupleMixin:
+class EqualityTupleMixin(object):
 
     def _equality_tuple(self):
         raise NotImplementedError('_equality_tuple method not implemented')
