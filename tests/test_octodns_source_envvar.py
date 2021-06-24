29532302e28fd16886d782b5eb9544f62b02ea46
blanariu <blanariu@adobe.com>
2021-06-24 19:28:53 +0300

    Leave just importing from mock

--- tests/test_octodns_source_envvar.py
+++ tests/test_octodns_source_envvar.py
@@ -1,12 +1,6 @@
+from mock import patch
 from six import text_type
 from unittest import TestCase
-
-try:
-    # Python 3
-    from unittest.mock import patch
-except ImportError:
-    # Python 2
-    from mock import patch
 
 from octodns.source.envvar import EnvVarSource
 from octodns.source.envvar import EnvironmentVariableNotFoundException
