3ed7a88e343c89b7153efea25db1b6287b2f0823
Solvik Blum <solvik@numberly.com>
2023-11-10 13:58:08 +0100

    add test to cover CIDR validation in config for filters

--- tests/test_octodns_processor_filter.py
+++ tests/test_octodns_processor_filter.py
@@ -194,6 +194,12 @@ class TestNetworkValueFilter(TestCase):
     ]:
         zone.add_record(record)
 
+    def test_bad_config(self):
+        with self.assertRaises(ValueError):
+            filter_private = NetworkValueRejectlistFilter(
+                'rejectlist', set(('string', '42.42.42.42/43'))
+            )
+
     def test_reject(self):
         filter_private = NetworkValueRejectlistFilter(
             'rejectlist', set(('10.0.0.0/8', 'fd00::/8'))
