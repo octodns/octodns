b6362ec6d0fb33f594bc1b839acfd05d08eb02ad
Ross McFarland <rwmcfa1@gmail.com>
2024-03-20 13:49:33 -0700

    Update black to 24.x

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ setup(
             # likely to result in formatting changes that should happen in their
             # own PR. This will basically happen yearly
             # https://black.readthedocs.io/en/stable/the_black_code_style/index.html#stability-policy
-            'black>=23.1.0,<24.0.0',
+            'black>=24.3.0,<25.0.0',
             'build>=0.7.0',
             'isort>=5.11.5',
             'pycountry>=19.8.18',
