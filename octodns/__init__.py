4035afaa834d61f7282fce42819e44b39e7aa56a
Ross McFarland <rwmcfa1@gmail.com>
2024-04-29 14:41:42 -0700

    Version 1.7.0 bump & changelog update

--- octodns/__init__.py
+++ octodns/__init__.py
@@ -1,4 +1,4 @@
 'OctoDNS: DNS as code - Tools for managing DNS across multiple providers'
 
 # TODO: remove __VERSION__ w/2.x
-__version__ = __VERSION__ = '1.6.1'
+__version__ = __VERSION__ = '1.7.0'
