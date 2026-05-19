ebb72e3aa306aaf315c2828af3aab9f45f66a4f5
Ross McFarland <rwmcfa1@gmail.com>
2026-05-19 12:34:41 -0700

    Register NS validators in zone package init
    Import GlueForInZoneNsZoneValidator, MultiValueNsZoneValidator, and NsTargetNotCnameZoneValidator inside octodns/zone/__init__.py.
    
    This fixes a bug where none of the NS-related zone validators were actually imported or registered in production, which was a pre-existing issue for the first two and also impacted the newly added ns-target-not-cname validator in this branch.

--- octodns/zone/__init__.py
+++ octodns/zone/__init__.py
@@ -6,15 +6,22 @@ from .base import DuplicateRecordException, InvalidNameError, Zone
 from .cname import CnameCoexistenceValidator
 from .cname_loops import NoCnameLoopZoneValidator
 from .mail import MailZoneValidator
+from .ns import (
+    GlueForInZoneNsZoneValidator,
+    MultiValueNsZoneValidator,
+    NsTargetNotCnameZoneValidator,
+)
 from .srv import SrvTargetNotCnameZoneValidator
 from .subzone import SubzoneRecordValidator
 
 CnameCoexistenceValidator
 DuplicateRecordException
+GlueForInZoneNsZoneValidator
 InvalidNameError
 MailZoneValidator
+MultiValueNsZoneValidator
 NoCnameLoopZoneValidator
-SubzoneRecordValidator
-
+NsTargetNotCnameZoneValidator
 SrvTargetNotCnameZoneValidator
+SubzoneRecordValidator
 Zone
