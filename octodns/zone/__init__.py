634b7bb5eab55da571420503cbedca3801519641
Ross McFarland <rwmcfa1@gmail.com>
2026-05-23 08:35:23 -0700

    Fix bad conflict resolution

--- octodns/zone/__init__.py
+++ octodns/zone/__init__.py
@@ -1,7 +1,7 @@
 #
 
 from .base import DuplicateRecordException, InvalidNameError, Zone
-<<<<<<< resolvable-in-zone
+from .caa import CaaZoneValidator
 from .cname import (
     CnameCoexistenceValidator,
     CnameTargetResolvableInZoneZoneValidator,
@@ -9,12 +9,6 @@ from .cname import (
 )
 from .mail import MailZoneValidator, MxTargetResolvableInZoneZoneValidator
 from .srv import SrvTargetResolvableInZoneZoneValidator
-=======
-from .caa import CaaZoneValidator
-from .cname import CnameCoexistenceValidator
-from .cname_loops import NoCnameLoopZoneValidator
-from .mail import MailZoneValidator
->>>>>>> main
 from .subzone import SubzoneRecordValidator
 
 CnameCoexistenceValidator
